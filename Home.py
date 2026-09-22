5b9035fff9faed1ff74cba52a4ef8b11150fd6d5
KaiyunLi115-tech <kaiyunli258@gmail.com>
2026-09-22 20:22:07 +0800

    Add Streamlit Home page

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="World Cities", page_icon="🌍")
+
+st.title("🌍 World Cities")
+st.write("Welcome to the World Cities Streamlit app!")
+st.write("Explore cities from around the world using the app.")
+
+if st.button("Go to the app"):
+    st.switch_page("pages/app.py")
